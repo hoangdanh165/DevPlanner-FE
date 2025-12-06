9aa647163db99bf8f9e8e902f423daddc37d8618
hoangdanh165 <hoangdanh.165@gmail.com>
2025-12-06 13:52:14 +0700

    fix: add domain to allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: true,
-    host: "0.0.0.0",
+    host: true,
+    allowedHosts: ["devplanner.duckdns.org", "localhost"],
   },
   resolve: {
     alias: {
